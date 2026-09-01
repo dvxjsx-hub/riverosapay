725a74b5548b23592ab3028238aa72863faa3e8b
riverojsx <dvxjsx@gmail.com>
2026-09-01 15:05:36 -0500

    Seguridad: exigir sesión para las operaciones de Trabajo

--- src/routes/trabajo.routes.js
+++ src/routes/trabajo.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const trabajo = require('../controllers/trabajo.controller');
 const ah = require('../middleware/asyncHandler');
+const { requireUser } = require('../middleware/session');
 
 const router = express.Router();
+router.use(requireUser);
 
 router.get('/mis-jefes/:empleadoId', ah(trabajo.obtenerMisJefes));
 router.get('/trabajo/jefe/:jefeId', ah(trabajo.obtenerTrabajosComoJefe));
-
 router.get('/trabajo/:empleadoId', ah(trabajo.obtenerSnapshot));
 router.post('/trabajo/:empleadoId/turnos', ah(trabajo.crearTurno));
 router.patch('/trabajo/turnos/:turnoId', ah(trabajo.actualizarTurno));
