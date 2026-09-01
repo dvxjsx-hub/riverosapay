89ef4271e272431fff79620da3e6316c8a109f95
riverojsx <dvxjsx@gmail.com>
2026-09-01 15:19:26 -0500

    Tanda 3: proteger canales Socket.IO con sesión

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,15 +1,26 @@
 const verificacion = require('../models/verificacion.model');
+const { readSession } = require('../middleware/session');
 
 function initSockets(io) {
+  io.use((socket, next) => {
+    const session = readSession(socket.request);
+    if (!session || session.type !== 'user' || !session.userId) return next(new Error('Sesión de usuario requerida.'));
+    socket.userId = session.userId;
+    next();
+  });
+
   io.on('connection', (socket) => {
-    socket.on('register-empleado', ({ empleadoId }) => {
-      if (empleadoId) socket.join('emp-' + empleadoId);
+    socket.on('register-empleado', ({ empleadoId } = {}) => {
+      if (empleadoId === socket.userId) socket.join('emp-' + socket.userId);
     });
-    socket.on('register-jefe', ({ jefeId }) => {
-      if (jefeId) socket.join('jefe-' + jefeId);
+
+    socket.on('register-jefe', ({ jefeId } = {}) => {
+      if (jefeId === socket.userId) socket.join('jefe-' + socket.userId);
     });
-    socket.on('jefe-ver-empleado', ({ jefeId, empleadoId }) => {
-      if (verificacion.tieneAcceso(jefeId, empleadoId)) socket.join('emp-' + empleadoId);
+
+    socket.on('jefe-ver-empleado', ({ jefeId, empleadoId } = {}) => {
+      if (jefeId !== socket.userId) return;
+      if (verificacion.tieneAcceso(socket.userId, empleadoId)) socket.join('emp-' + empleadoId);
     });
   });
 }
