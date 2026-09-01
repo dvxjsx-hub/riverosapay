f1bdb0a04a338ab31b29e9011f3f9a1eea9ea2d9
riverojsx <dvxjsx@gmail.com>
2026-09-01 14:28:39 -0500

    feat(admin): protect administration routes

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const admin = require('../controllers/admin.controller');
+const ah = require('../middleware/asyncHandler');
+const { requireAdmin } = require('../middleware/session');
+
+const router = express.Router();
+router.use(requireAdmin);
+router.get('/cuentas', ah(admin.cuentas));
+router.delete('/cuentas/:userId', ah(admin.eliminar));
+
+module.exports = router;
