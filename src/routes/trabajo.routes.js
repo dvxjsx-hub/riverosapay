b2ca73b0cee21f21f4255ccb8c20d1ab8c518347
riverojsx <dvxjsx@gmail.com>
2026-09-02 09:16:32 -0500

    Tanda 5: añadir endpoint explícito para finalizar trabajos

--- src/routes/trabajo.routes.js
+++ src/routes/trabajo.routes.js
@@ -11,6 +11,7 @@ router.get('/trabajo/jefe/:jefeId', ah(trabajo.obtenerTrabajosComoJefe));
 router.get('/trabajo/:empleadoId', ah(trabajo.obtenerSnapshot));
 router.post('/trabajo/:empleadoId/turnos', ah(trabajo.crearTurno));
 router.patch('/trabajo/turnos/:turnoId', ah(trabajo.actualizarTurno));
+router.post('/trabajo/turnos/:turnoId/finalizar', ah(trabajo.finalizarTurno));
 router.delete('/trabajo/turnos/:turnoId', ah(trabajo.eliminarTurno));
 router.post('/trabajo/turnos/:turnoId/confirmar-eliminacion', ah(trabajo.confirmarEliminacion));
 router.post('/trabajo/turnos/:turnoId/rechazar-eliminacion', ah(trabajo.rechazarEliminacion));
