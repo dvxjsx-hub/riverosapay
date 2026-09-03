ad937c6d1e78dee9b3c319ba9b62e8ca72f57b84
riverojsx <dvxjsx@gmail.com>
2026-09-03 17:59:19 -0500

    Añadir ruta admin para verificación de cuentas

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,6 +6,7 @@ const { requireAdmin } = require('../middleware/session');
 const router = express.Router();
 router.use(requireAdmin);
 router.get('/cuentas', ah(admin.cuentas));
+router.patch('/cuentas/:userId/verificada', ah(admin.cambiarVerificacion));
 router.delete('/cuentas/:userId', ah(admin.eliminar));
 
 module.exports = router;
