f2e6f13625d56f46975409742febc698921d3291
riverojsx <dvxjsx@gmail.com>
2026-09-03 10:36:38 -0500

    Bloque 5: separar registro de pagos del empleado

--- src/routes/trabajo.routes.js
+++ src/routes/trabajo.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const trabajo = require('../controllers/trabajo.controller');
+const trabajoPago = require('../controllers/trabajoPago.controller');
 const ah = require('../middleware/asyncHandler');
 const { requireUser } = require('../middleware/session');
+const { bloquearCambioJefe } = require('../middleware/inmutabilidadTrabajo');
 
 const router = express.Router();
 router.use(requireUser);
@@ -12,7 +14,8 @@ router.post('/trabajo/jefe/:jefeId/solicitudes', ah(trabajo.crearSolicitudTrabaj
 router.post('/trabajo/solicitudes/:solicitudId/responder', ah(trabajo.responderSolicitudTrabajo));
 router.get('/trabajo/:empleadoId', ah(trabajo.obtenerSnapshot));
 router.post('/trabajo/:empleadoId/turnos', ah(trabajo.crearTurno));
-router.patch('/trabajo/turnos/:turnoId', ah(trabajo.actualizarTurno));
+router.patch('/trabajo/turnos/:turnoId', bloquearCambioJefe, ah(trabajo.actualizarTurno));
+router.patch('/trabajo/turnos/:turnoId/pago-empleado', ah(trabajoPago.actualizarPagoEmpleado));
 router.post('/trabajo/turnos/:turnoId/finalizar', ah(trabajo.finalizarTurno));
 router.delete('/trabajo/turnos/:turnoId', ah(trabajo.eliminarTurno));
 router.post('/trabajo/turnos/:turnoId/confirmar-eliminacion', ah(trabajo.confirmarEliminacion));
