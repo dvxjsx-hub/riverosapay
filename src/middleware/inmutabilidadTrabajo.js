4e1289a4973c7ba3bc31d43ef0ed1d8b4f0d6c4f
riverojsx <dvxjsx@gmail.com>
2026-09-03 11:01:27 -0500

    Actualización 5.5: validar estado congelado en middleware

--- src/middleware/inmutabilidadTrabajo.js
+++ src/middleware/inmutabilidadTrabajo.js
@@ -1,3 +1,9 @@
+const trabajo = require('../models/trabajo.model');
+
+function obtenerTurno(req) {
+  return trabajo.buscarTurnoPorId(req.params.turnoId);
+}
+
 function bloquearCambioJefe(req, res, next) {
   if (req.body && Object.prototype.hasOwnProperty.call(req.body, 'jefeAsignadoId')) {
     return res.status(400).json({ error: 'El jefe asignado queda fijado al crear el trabajo y no puede cambiarse.' });
@@ -6,7 +12,7 @@ function bloquearCambioJefe(req, res, next) {
 }
 
 function bloquearTrabajoCongelado(req, res, next) {
-  const turno = req.trabajoTurno;
+  const turno = obtenerTurno(req);
   if (turno && turno.congelado === true) {
     return res.status(409).json({ error: 'Este trabajo está congelado y ya no admite cambios.' });
   }
@@ -14,7 +20,7 @@ function bloquearTrabajoCongelado(req, res, next) {
 }
 
 function bloquearPagoEmpleadoFinalizado(req, res, next) {
-  const turno = req.trabajoTurno;
+  const turno = obtenerTurno(req);
   if (turno && turno.finalizado === true) {
     return res.status(409).json({ error: 'Este trabajo ya finalizó y el empleado no puede editar el pago.' });
   }
