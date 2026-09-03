f8b55a8a8b99cb17226930b189057b01b543a7b0
riverojsx <dvxjsx@gmail.com>
2026-09-02 23:57:08 -0500

    TANDA 2: rutas de gestión de capital

--- src/routes/tesoreria.routes.js
+++ src/routes/tesoreria.routes.js
@@ -2,14 +2,15 @@ const express = require('express');
 const tesoreria = require('../controllers/tesoreria.controller');
 const ah = require('../middleware/asyncHandler');
 const { requireUser } = require('../middleware/session');
-
 const router = express.Router();
 router.use(requireUser);
-
 router.get('/jefe/:jefeId/tesoreros', ah(tesoreria.listarMisTesoreros));
 router.post('/jefe/:jefeId/tesoreros/solicitudes', ah(tesoreria.crearSolicitudTesorero));
 router.get('/tesorero/:tesoreroId/solicitudes', ah(tesoreria.listarSolicitudesRecibidas));
 router.post('/tesorero/solicitudes/:solicitudId/responder', ah(tesoreria.responderSolicitudTesorero));
 router.get('/tesorero/:tesoreroId/relaciones', ah(tesoreria.listarMisRelaciones));
-
+router.get('/jefe/:jefeId/tesoreros/:tesoreroId/movimientos', ah(tesoreria.listarMovimientosJefe));
+router.post('/jefe/:jefeId/tesoreros/:tesoreroId/capital', ah(tesoreria.agregarCapital));
+router.get('/tesorero/:tesoreroId/movimientos', ah(tesoreria.listarMovimientosTesorero));
+router.post('/tesorero/gastos', ah(tesoreria.registrarGasto));
 module.exports = router;
